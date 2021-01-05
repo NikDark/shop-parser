f857287b55f32ff62dcf66e060b92ef7f2476208
Nikita Efremov <nikefremov.00.008@hotmail.com>
2021-01-05 11:12:01 +0300

    Generated SQL scripts for tables (PostgreSQL)

---
+++ price_parser.sql
@@ -0,0 +1,30 @@
+-- Table: public.price_parser
+
+-- DROP TABLE public.price_parser;
+
+CREATE TABLE public.price_parser
+(
+    id integer NOT NULL,
+    date date NOT NULL,
+    price double precision NOT NULL,
+    id_product integer NOT NULL,
+    CONSTRAINT price_parser_pkey PRIMARY KEY (id),
+    CONSTRAINT id_product_key FOREIGN KEY (id_product)
+        REFERENCES public.product_parser (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.price_parser
+    OWNER to postgres;
+-- Index: fki_id_product_key
+
+-- DROP INDEX public.fki_id_product_key;
+
+CREATE INDEX fki_id_product_key
+    ON public.price_parser USING btree
+    (id_product ASC NULLS LAST)
+    TABLESPACE pg_default;
---
+++ product_parser.sql
@@ -0,0 +1,18 @@
+-- Table: public.product_parser
+
+-- DROP TABLE public.product_parser;
+
+CREATE TABLE public.product_parser
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    shop integer NOT NULL,
+    link text COLLATE pg_catalog."default" NOT NULL,
+    authorize boolean NOT NULL,
+    CONSTRAINT product_parser_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.product_parser
+    OWNER to postgres;
